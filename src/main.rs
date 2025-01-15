a033bd1ac933384b7b80628b79dabecc5cf9e0a9
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2025-01-15 08:37:08 -0300

    Refactor to use merged_range2, with better data model. Major simplification in code. However it panics, lol

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
-use itertools::Itertools;
+use merged_range2::MergedRange;
 use pulldown_cmark::{Event, Options, Parser, Tag, TagEnd};
 
 use std::env;
 use std::fs;
-use std::mem;
 use std::ops::Range;
-
-#[derive(Debug)]
-struct Span<I> {
-    element: I,
-    range: Range<usize>,
-}
-
-impl<I> Span<I> {
-    fn new(element: I, range: Range<usize>) -> Self {
-        Span { element, range }
-    }
-}
-
-#[derive(Debug)]
-enum Content {
-    RawMarkdown(Span<()>),
-    NestedList(Span<List>),
-}
 
 macro_rules! print_helper {
     ($ident: expr, $text: expr) => {
@@ -33,30 +14,24 @@ macro_rules! print_helper {
     };
 }
 
-impl Content {
-    fn print(&self, source: &str, ident: usize, verbose: bool) {
-        match self {
-            Content::RawMarkdown(span) => {
-                print_helper!(ident, &source[span.range.clone()]);
-            }
-            Content::NestedList(list) => {
-                list.print(source, ident, verbose);
-            }
-        }
-    }
+#[derive(Debug)]
+struct Span<I> {
+    element: I,
+    range: Range<usize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Item {
     checkbox: Option<Span<bool>>,
-    contents: Vec<Content>,
+    contents: MergedRange<usize>,
+    nested_list: Option<Span<List>>,
 }
 
 impl Item {
-    fn new() -> Self {
-        Item {
-            checkbox: None,
-            contents: vec![],
+    fn span(self, range: Range<usize>) -> Span<Self> {
+        Span {
+            element: self,
+            range,
         }
     }
 }
@@ -68,8 +43,8 @@ impl Span<Item> {
             ident,
             self.element.checkbox.as_ref().map(|x| x.element)
         );
-        for content in &self.element.contents {
-            content.print(source, ident + 2, verbose);
+        for range in &self.element.contents {
+            print_helper!(ident + 2, &source[range.clone()]);
         }
         if verbose {
             println!("(item source: {:?})", &source[self.range.clone()]);
@@ -78,59 +53,16 @@ impl Span<Item> {
     }
 }
 
-impl Item {
-    fn merge_texts(&mut self) {
-        let contents = mem::take(&mut self.contents);
-        let contents = contents
-            .into_iter()
-            .coalesce(|x, y| match (x, y) {
-                (Content::RawMarkdown(x), Content::RawMarkdown(y)) => {
-                    let should_merge = x.range.end == y.range.start;
-
-                    if should_merge {
-                        Ok(Content::RawMarkdown(Span::new(
-                            (),
-                            x.range.start..y.range.end,
-                        )))
-                    } else {
-                        Err((Content::RawMarkdown(x), Content::RawMarkdown(y)))
-                    }
-                }
-                (Content::NestedList(mut list1), Content::NestedList(mut list2)) => {
-                    list1.element.merge_texts();
-                    list2.element.merge_texts();
-
-                    Err((Content::NestedList(list1), Content::NestedList(list2)))
-                }
-                (Content::NestedList(mut list), y) => {
-                    list.element.merge_texts();
-
-                    Err((Content::NestedList(list), y))
-                }
-                (x, Content::NestedList(mut list)) => {
-                    list.element.merge_texts();
-
-                    Err((x, Content::NestedList(list)))
-                }
-            })
-            .collect::<Vec<_>>();
-        self.contents = contents;
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct List {
     items: Vec<Span<Item>>,
 }
 
 impl List {
-    fn new() -> Self {
-        List { items: vec![] }
-    }
-
-    fn merge_texts(&mut self) {
-        for Span { element, .. } in &mut self.items {
-            element.merge_texts();
+    fn span(self, range: Range<usize>) -> Span<Self> {
+        Span {
+            element: self,
+            range,
         }
     }
 }
@@ -148,8 +80,13 @@ impl Span<List> {
     }
 }
 
-fn parse(input: &str) -> Vec<Span<List>> {
-    let mut top_level_lists = vec![];
+#[derive(Debug, Default)]
+struct Board {
+    lists: Vec<Span<List>>,
+}
+
+fn parse(input: &str) -> Board {
+    let mut board = Board::default();
 
     let mut list_stack: Vec<List> = vec![];
 
@@ -162,7 +99,7 @@ fn parse(input: &str) -> Vec<Span<List>> {
             Event::Start(Tag::List(None)) => {
                 println!("Found unordered list start");
 
-                list_stack.push(List::new());
+                list_stack.push(List::default());
             }
             Event::End(TagEnd::List(false)) => {
                 println!("Found unordered list end");
@@ -170,58 +107,56 @@ fn parse(input: &str) -> Vec<Span<List>> {
                 let current_list = list_stack.pop().unwrap();
 
                 if let Some(current_item) = item_stack.last_mut() {
-                    current_item
-                        .contents
-                        .push(Content::NestedList(Span::new(current_list, range)));
+                    if current_item.nested_list.is_some() {
+                        panic!("a list item should not have two sub lists");
+                    }
+                    current_item.nested_list = Some(current_list.span(range));
                 } else {
-                    top_level_lists.push(Span::new(current_list, range));
+                    board.lists.push(current_list.span(range));
                 }
             }
             Event::Start(Tag::Item) => {
                 println!("Found item start");
 
-                item_stack.push(Item::new());
+                item_stack.push(Item::default());
             }
             Event::End(TagEnd::Item) => {
                 println!("Found item end");
 
                 let current_list = list_stack.last_mut().unwrap();
                 let current_item = item_stack.pop().unwrap();
-                current_list.items.push(Span::new(current_item, range));
+                current_list.items.push(current_item.span(range));
             }
             Event::TaskListMarker(marked) => {
                 println!("Found task list marker: {marked}");
 
                 let current_item = item_stack.last_mut().unwrap();
-                current_item.checkbox = Some(Span::new(marked, range));
+                current_item.checkbox = Some(Span {
+                    element: marked,
+                    range,
+                });
             }
             _ => {
                 if let Some(current_item) = item_stack.last_mut() {
                     println!("Found something else inside item");
-                    current_item
-                        .contents
-                        .push(Content::RawMarkdown(Span::new((), range)));
+                    current_item.contents.insert_range(&range);
                 }
             }
         }
     }
 
-    for list in top_level_lists.iter_mut() {
-        list.element.merge_texts();
-    }
-
-    top_level_lists
+    board
 }
 
 fn main() {
     let file = env::args().nth(1).unwrap_or(String::from("TODO.md"));
     let markdown = fs::read_to_string(file).unwrap();
 
-    let x = parse(&markdown);
+    let board = parse(&markdown);
 
     println!("");
 
-    for list in x {
+    for list in &board.lists {
         list.print(&markdown, 0, false);
         println!();
         println!();
