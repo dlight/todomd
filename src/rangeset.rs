0e608988696a82602cee1a96814e5e0be1990915
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2025-02-02 07:24:59 -0300

    Add `range_difference` method, also tidy up internal `Range` api

--- src/rangeset.rs
+++ src/rangeset.rs
@@ -1,49 +1,57 @@
 use std::borrow::Borrow;
+use std::ops::Bound;
+
+use itertools::{Itertools, Position};
 
 /// A range of indices. It's assumed that the range is non-empty. This is normally done using the
 /// newtype pattern, but this would lead to some boilerplate (like calling `range.into` or similar)
 /// so we just use a type alias. This means that each function that receives a range first has to
 /// assert the range is not empty.
+///
+/// (Maybe I should work with empty ranges as well)
 pub type Range = std::ops::Range<usize>;
 
-/// I think this won't actually be used, might delete later.
-fn intersect_ranges(range1: impl Borrow<Range>, range2: impl Borrow<Range>) -> Option<Range> {
-    let range1: &Range = range1.borrow();
-    let range2: &Range = range2.borrow();
-    assert!(!range1.is_empty());
-    assert!(!range2.is_empty());
-    let start = range1.start.max(range2.start);
-    let end = range1.end.min(range2.end);
-    if start < end {
-        Some(start..end)
-    } else {
-        None
-    }
+trait RangeExt {
+    /// Merge ranges. If ranges are adjacent or overlapping, returns a single range that covers them. If they are not, returns None.
+    fn merge_range(&self, range: impl Borrow<Self>) -> Option<Self>
+    where
+        Self: Sized;
+
+    fn range_difference(&self, range: impl Borrow<Self>) -> Vec<Self>
+    where
+        Self: Sized;
 }
 
-/// Merge ranges. If ranges are adjacent or overlapping, merge them, If they are not, return the original ranges.
-fn merge_ranges(range1: impl Borrow<Range>, range2: impl Borrow<Range>) -> Option<Range> {
-    let range1: &Range = range1.borrow();
-    let range2: &Range = range2.borrow();
-    assert!(!range1.is_empty());
-    assert!(!range2.is_empty());
-    let start = range1.start.min(range2.start);
-    let end = range1.end.max(range2.end);
-    if range1.end < range2.start || range2.end < range1.start {
-        None
-    } else {
-        Some(start..end)
+impl RangeExt for Range {
+    fn merge_range(&self, range: impl Borrow<Self>) -> Option<Self> {
+        let range: &Range = range.borrow();
+        assert!(!self.is_empty());
+        assert!(!range.is_empty());
+        let start = self.start.min(range.start);
+        let end = self.end.max(range.end);
+        if self.end < range.start || range.end < self.start {
+            None
+        } else {
+            Some(start..end)
+        }
     }
-}
 
-/// Assumes that the range in the first argument overlaps all ranges in the second argument.
-fn merge_ranges_vec(range: impl Borrow<Range>, vec: Vec<impl Borrow<Range>>) -> Option<Range> {
-    let range: &Range = range.borrow();
-    let mut range = range.clone();
-    for r in vec {
-        range = merge_ranges(range, r)?;
+    fn range_difference(&self, range: impl Borrow<Self>) -> Vec<Self> {
+        let range: &Range = range.borrow();
+        assert!(!self.is_empty());
+        assert!(!range.is_empty());
+        if self.end < range.start || self.start > range.end {
+            return vec![self.clone()];
+        } else if self.start < range.start && self.end > range.end {
+            return vec![self.start..range.start, range.end..self.end];
+        } else if self.start < range.start {
+            return vec![self.start..range.start];
+        } else if self.end > range.end {
+            return vec![range.end..self.end];
+        } else {
+            return vec![];
+        }
     }
-    Some(range)
 }
 
 /// A set of non-overlapping, non-adjacent ranges. Inserting new ranges will merge them if
@@ -73,10 +81,6 @@ impl FromIterator<Range> for RangeSet {
 
 impl RangeSet {
     pub fn insert_range(&mut self, range: impl Borrow<Range>) {
-        use itertools::Itertools;
-        use itertools::Position;
-        use std::ops::Bound;
-
         let q: &str;
         let r: &str;
 
@@ -117,7 +121,7 @@ impl RangeSet {
             .filter_map(|(p, r)| (!matches!(p, Position::Middle)).then_some(r));
 
         for inner_range in endpoints {
-            new_range = merge_ranges(&new_range, inner_range).unwrap();
+            new_range = new_range.merge_range(inner_range).unwrap();
         }
 
         self.contents.splice((start, end), [new_range]);
@@ -125,10 +129,6 @@ impl RangeSet {
 
     /// TODO: make it work
     pub fn remove_range(&mut self, removed_range: impl Borrow<Range>) {
-        use itertools::Itertools;
-        use itertools::Position;
-        use std::ops::Bound;
-
         let removed_range: &Range = removed_range.borrow();
 
         if self.contents.is_empty() {
@@ -245,29 +245,16 @@ mod tests {
     use super::*;
     use rand::prelude::*;
 
-    /// Test if ranges intersect correctly.
-    #[test]
-    fn test_intersect_ranges() {
-        assert_eq!(intersect_ranges(0..5, 5..10), None);
-        assert_eq!(intersect_ranges(0..5, 6..10), None);
-        assert_eq!(intersect_ranges(0..5, 4..10), Some(4..5));
-        assert_eq!(intersect_ranges(0..5, 0..10), Some(0..5));
-        assert_eq!(intersect_ranges(0..5, 0..5), Some(0..5));
-        assert_eq!(intersect_ranges(0..3, 1..4), Some(1..3));
-        assert_eq!(intersect_ranges(0..3, 2..5), Some(2..3));
-        assert_eq!(intersect_ranges(0..1, 1..3), None);
-    }
-
     #[test]
     fn test_merge_ranges() {
-        assert_eq!(merge_ranges(1..2, 0..1), Some(0..2));
-        assert_eq!(merge_ranges(0..5, 5..10), Some(0..10));
-        assert_eq!(merge_ranges(0..5, 6..10), None);
-        assert_eq!(merge_ranges(0..5, 5..10), Some(0..10));
-        assert_eq!(merge_ranges(0..5, 5..10), Some(0..10));
-        assert_eq!(merge_ranges(430..888, 602..835), Some(430..888));
+        assert_eq!((1..2).merge_range(0..1), Some(0..2));
+        assert_eq!((0..5).merge_range(5..10), Some(0..10));
+        assert_eq!((0..5).merge_range(&(6..10)), None);
+        assert_eq!((0..5).merge_range(5..10), Some(0..10));
+        assert_eq!((0..5).merge_range(&(5..10)), Some(0..10));
+        assert_eq!((430..888).merge_range(602..835), Some(430..888));
         assert_eq!(
-            merge_ranges(usize::MAX - 1..usize::MAX, usize::MAX - 2..usize::MAX - 1),
+            (usize::MAX - 1..usize::MAX).merge_range(&(usize::MAX - 2..usize::MAX - 1)),
             Some(usize::MAX - 2..usize::MAX)
         );
     }
@@ -286,7 +273,7 @@ mod tests {
                 continue;
             }
             dbg!(&range1, &range2);
-            let merged = merge_ranges(range1.clone(), range2.clone());
+            let merged = range1.merge_range(range2);
             if start1 < start2 {
                 if end1 >= start2 {
                     if end2 >= end1 {
@@ -312,15 +299,26 @@ mod tests {
     }
 
     #[test]
-    fn test_merge_ranges_vec() {
+    fn range_difference() {
+        assert_eq!((1..5).range_difference(0..1), vec![1..5]);
+        assert_eq!((1..5).range_difference(0..2), vec![2..5]);
+        assert_eq!((1..5).range_difference(2..3), vec![1..2, 3..5]);
+        assert_eq!((1..5).range_difference(3..5), vec![1..3]);
+        assert_eq!((1..5).range_difference(4..5), vec![1..4]);
+        assert_eq!((1..5).range_difference(5..6), vec![1..5]);
+        assert_eq!((1..5).range_difference(0..6), vec![]);
+        assert_eq!((1..5).range_difference(2..4), vec![1..2, 4..5]);
+        assert_eq!((1..5).range_difference(1..5), vec![]);
+        assert_eq!((1..5).range_difference(0..5), vec![]);
+        assert_eq!((1..5).range_difference(0..6), vec![]);
+        assert_eq!((1..5).range_difference(1..2), vec![2..5]);
+        assert_eq!((1..5).range_difference(4..5), vec![1..4]);
         assert_eq!(
-            merge_ranges_vec(0..5, vec![5..10, 1..6, 0..5, 1..2]),
-            Some(0..10)
+            (usize::MAX - 1..usize::MAX).range_difference(0..1),
+            vec![usize::MAX - 1..usize::MAX]
         );
-        assert_eq!(
-            merge_ranges_vec(0..5, vec![5..10, 1..6, 0..5, 1..2, 11..12]),
-            None
-        );
+        assert_eq!((1..2).range_difference(1..2), vec![]);
+        assert_eq!((0..usize::MAX).range_difference(0..usize::MAX), vec![]);
     }
 
     #[test]
@@ -385,10 +383,8 @@ mod tests {
 
     #[test]
     fn test_remove_rangeset() {
-        let mut ranges = RangeSet::from(vec![1..5, 10..15, 20..25, 2..3, 12..13, 22..23]);
-        ranges.remove_range(1..5);
-        assert_eq!(ranges.contents, vec![5..10, 10..15, 20..25]);
-        ranges.remove_range(10..15);
-        assert_eq!(ranges.contents, vec![5..10, 20..25]);
+        let mut ranges = RangeSet::from(vec![1..5, 10..15, 20..25]);
+        ranges.remove_range(4..22);
+        assert_eq!(ranges.contents, vec![1..4, 22..25]);
     }
 }
